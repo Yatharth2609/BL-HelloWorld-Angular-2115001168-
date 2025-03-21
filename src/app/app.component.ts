dd081963fc1423faa4499c98786f72c0e7c1b8bb
Yatharth2609 <yatharth.mishra2002@gmail.com>
2025-03-21 22:50:31 +0530

    [Yatharth] Add: Modified app component to redirect to bridgeLabz url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HelloWorld';
-  imgUrl="../assets/BL_logo_square_jpg.jpg"
+  imgUrl="../assets/BL_logo_square_jpg.jpg";
+  url="https://www.bridgelabz.com"
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz!!!."
+  }
+
+  onClick($event: any) {
+    console.log("Save button is clicked", $event);
+    window.open(this.url, "_blank");
   }
 }
